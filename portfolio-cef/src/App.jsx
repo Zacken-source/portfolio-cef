2f505fefbc7d3d3ad21f352b5e1467b911108bf7
zacken <mchereau05@gmail.com>
2025-12-29 16:42:28 +0100

    verification of breakpoint, browser, form, commentaire, clean, env and checklist
    Closes #20, #21, #22, #23, #24, #25, #26

--- portfolio-cef/src/App.jsx
+++ portfolio-cef/src/App.jsx
@@ -9,6 +9,7 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import LegalNotice from './pages/LegalNotice';
 
+
 import Footer from './components/layout/Footer';
 
 export default function App() {
